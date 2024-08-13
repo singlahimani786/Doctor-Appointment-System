e07b93ef12fe705d55d1042baa38ccd00206613e
singlahimani786 <singlahimani786@gmail.com>
2024-08-13 12:34:19 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/doctorapp");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
